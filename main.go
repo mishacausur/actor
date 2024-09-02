e455645b4803006740aa311e1d01df118dc2cfab
Misha Causur <mishacausur@mail.ru>
2024-09-03 01:01:20 +0500

    .6

--- main.go
+++ main.go
@@ -5,16 +5,21 @@ import "fmt"
 var print = fmt.Println
 
 func main() {
-	var l, r int
-	fmt.Scan(&l, &r)
+	var l int
+	_, err := fmt.Scan(&l)
 
-	if l == 0 || r == 0 {
-		print("one of them is zero")
-	} else if l < 0 && r < 0 {
-		print("both are negative")
-	} else if l > 0 && r > 0 {
-		print("both are positive")
+	if err != nil || l < 0 {
+		print("Некорректный ввод")
+		return
+	}
+
+	if l < 10 {
+		print("Число меньше 10")
+	} else if l < 100 {
+		print("Число меньше 100")
+	} else if l < 1000 {
+		print("Число меньше 1000")
 	} else {
-		print("one is positive and another on is negative")
+		print("Число больше или равно 1000")
 	}
 }
